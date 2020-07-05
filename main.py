80609f64f746d70e0a5e747a1cb4af4be685e764
M Hadi Sasmito <keselyoleren@gmail.com>
2020-07-05 20:40:37 +0700

    update converter .dcm to img_file

--- main.py
+++ main.py
@@ -1,19 +1,44 @@
 import cv2
 import pydicom
 import os
+import pylab
+import dicom
+import numpy as np
+from pydicom.pixel_data_handlers.util import convert_color_space
 
-file = 'img1.dcm'
+file = 'large.dcm'
 
-ds = pydicom.read_file(file)
-img = ds.pixel_array
+read_dcm_file = pydicom.dcmread(file)
 
-scale_percent = 50 # percent of original size
-width = int(img.shape[1] * scale_percent / 100)
-height = int(img.shape[0] * scale_percent / 100)
-dim = (width, height)
+img_arr = read_dcm_file.pixel_array
 
-resized = cv2.resize(img, dim)
+shape = img_arr.shape
+image_2d = img_arr.astype(float)
 
-print(dim)
+dimention = 0
+if len(shape) == 4:
+    dimension = shape[1]
+    mid_frame = int(float(img_arr.shape[0]) / 2)
+    if img_arr.shape[3] == 3:
+        print(img_arr[mid_frame].shape)
+        imgexc_render = convert_color_space(img_arr[mid_frame], 'RGB', 'RGB')
+    image_2d = imgexc_render.astype(float)
+else:
+    dimension = shape[0]
 
-cv2.imwrite('result.jpg', resized)
+if dimension > 512 :
+    image_2d_scaled = 255 - (np.maximum(image_2d,0) / image_2d.max() * 255.0)
+else :
+    level = 600
+    window = 1600
+    if len(shape) == 4:
+        image_2d_scaled = (np.maximum(image_2d,0) / image_2d.max() * 255.0)
+    else : 
+        image_2d_scaled = np.piecewise(image_2d, 
+                        [image_2d <= (level - 0.5 - (window-1)/2),
+                            image_2d > (level - 0.5 + (window-1)/2)],
+                                [0, 255, lambda image_2d: ((image_2d - (level - 0.5))/(window-1) + 0.5)*(255-0)])
+
+image_2d_scaled = np.uint8(image_2d_scaled)
+
+cv2.imwrite('result.jpg', image_2d_scaled)
